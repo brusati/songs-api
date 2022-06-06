096d56b68e070532c1c002e319ec8628d28606dd
Ignacio Brusati <71679642+brusati@users.noreply.github.com>
2022-06-06 02:45:59 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+import pandas as pd
+
+songs = pd.read_csv('songs.csv')
+
+
+app = Flask(__name__)
+
+@app.route('/songs')
+def index():
+	return jsonify(list(songs['title']))
+
+@app.route('/ping')
+def ping():
+	return 'pong'
+
+@app.route('/artist/<string:anArtist>')
+def getSongsFrom(anArtist):
+	return jsonify(list(songs[songs['artist'] == anArtist]['title']))
+
+@app.route('/song/<string:aSongTitle>')
+def getDescriptionOf(aSongTitle):
+	return jsonify(list(songs[songs['title'] == aSongTitle]['description']))
